0f8c76186492d48c0efeaa2b434df14a27165e3e
Ronaldo <ronaldoanthony12@gmail.com>
2026-05-30 20:53:23 -0400

    Add Mockito ARRANGE on unit test 1

--- src/test/java/com/ronaldo/demo/BookUnitTest.java
+++ src/test/java/com/ronaldo/demo/BookUnitTest.java
@@ -6,6 +6,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.*;
+
+import static org.mockito.Mockito.*;
+
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +30,12 @@ public class BookUnitTest {
     private BookService bookService; //making a real bookserviuce obj, then pushing the fake bookrepo for it
 
     //arrange - the setup. setup the fake book, and say what it should do if the function runs
-    Long targetId = 1L;
-    Book fakeBook = new Book(targetId, "The Secret History", "Donna Tartt");
-
+    @Test
+    void testGetBookById_Success() {
+        //ARRANGE
+        Long targetId = 1L;
+        Book fakeBook = new Book(targetId, "The Secret History", "Donna Tartt");
+        Mockito.when(bookRepo.findById(targetId)).thenReturn(Optional.of(fakeBook)); //if we call the findbyid on fakebooks id it should return, well, fakebook lol
+    }
 
 }
