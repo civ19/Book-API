2d246d557350ab66e78ec7cfc11485ecef0a6f15
Ronaldo <ronaldoanthony12@gmail.com>
2026-06-01 09:05:29 -0400

    Add WebMvc Test 1 and ARRANGE

--- src/test/java/com/ronaldo/demo/BookControllerWebMvcTest.java
+++ src/test/java/com/ronaldo/demo/BookControllerWebMvcTest.java
@@ -1,4 +1,35 @@
 package com.ronaldo.demo;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(BookController.class) //annotation for webmvctest
 public class BookControllerWebMvcTest {
+    //inject web driver
+    @Autowired MockMvc mockMvc;
+
+    @MockBean //tells spring: create a fake bookrepo puppet and slip it inside the book continer
+    private BookRepository bookRepo;
+
+    @Test
+    public void testGetBookBy_Id_HappyPath() throws Exception {
+        //arrange: the setup of fake book and what it should do
+        Long testId = 1L;
+        Book testBook = new Book(testId, "City of Thieves", "David Benioff");
+        when(bookRepo.findById(testId)).thenReturn(Optional.of(testBook));
+
+        //Act and assert - Say the result, then check rules and assertions right after
+        mockMvc.perform()
+
+    }
 }
